f9d00f038ae7c29555f1f981328fd102b82780a2
Andrei Savin <mr.andr2012@gmail.com>
2023-01-07 21:00:02 +0300

    Решение задачи 2(Базовые алгоритмы)

---
+++ DZ_7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного 
+// числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int digitIndex = 2;
+if (number >= 100)
+{
+    int digit = number.ToString()[digitIndex] - '0';
+    Console.WriteLine(digit);
+}
+
+else
+{
+    Console.WriteLine("Такого числа нет");
+}
+
+
