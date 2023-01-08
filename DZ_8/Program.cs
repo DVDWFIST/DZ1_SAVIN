6c40698bbafe3dbcd8fed92f03af68a23431412f
Andrei Savin <mr.andr2012@gmail.com>
2023-01-09 00:24:37 +0300

    Задача с палиндромом

--- DZ_8/Program.cs
+++ DZ_8/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное 
 // число и проверяет, является ли оно палиндромом.
 
-void Palindrom ( int numbers )
+void Palindrom(int numbers)
 {
     if (numbers / 10000 == numbers % 10)
     {
         if ((numbers / 1000) % 10 == (numbers % 100) / 10)
-        Console.WriteLine("Палиндром");
+            Console.WriteLine("Палиндром");
     }
     else
     {
@@ -16,4 +16,4 @@ void Palindrom ( int numbers )
 
 Console.WriteLine("Введите пятизначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-Palindrom (number);
+Palindrom(number);
