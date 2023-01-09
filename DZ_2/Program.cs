d0c8c4f224b397f3a47ae873b3b79153f0dddd71
Andrei Savin <mr.andr2012@gmail.com>
2023-01-10 00:28:15 +0300

    Исправленно решене задачи на максимум из 3-х

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -1,19 +1,20 @@
-﻿Console.WriteLine("Введите первое число");
+﻿int Max(int num_1, int num_2, int num_3)
+{
+    int result = num_1;
+    if (num_2 > result) result = num_2;
+    if (num_3 > result) result = num_3;
+    return result;
+}
+
+Console.WriteLine("Введите первое число");
 int one_number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите первое число");
+Console.WriteLine("Введите второе число");
 int two_number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите первое число");
+Console.WriteLine("Введите третье число");
 int three_number = Convert.ToInt32(Console.ReadLine());
 
-int max = one_number;
+int max = Max(one_number, two_number, three_number);
 
-if (two_number > one_number)
-{
-    Console.WriteLine("Второе больше");
-}
-if (three_number > max)
-{
-    Console.WriteLine("Третье больше");
-}
+Console.WriteLine($"Максимальным из 3-х чисел является число:{max}");
