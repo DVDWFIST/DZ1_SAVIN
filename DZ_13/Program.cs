e818a52c7a8de64bcf15b05d3edad5e111fcfedd
Andrei Savin <mr.andr2012@gmail.com>
2023-01-15 02:23:20 +0300

    Решение задачи №29

---
+++ DZ_13/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Данные вводятся с консоли пользователем
+
+class ElementArray
+{
+
+    static void Main(string[] size)
+    {
+        Console.Write("Введите размерность массива:\t ");
+        
+        int countArray = int.Parse(Console.ReadLine());
+        int[] myArray = new int[countArray];
+
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            Console.Write($"\nВведите элемент массива под индексом {i}:\t ");
+            myArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("\nВывод массива: ");
+
+        for(int i = 0; i < myArray.Length; i++)
+        {
+            Console.Write(myArray[i] + ", ");
+        }
+
+    }
+}
+
+
+
+
+
+
